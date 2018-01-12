32da2cbaf3ef8077844eed32140f7472c4c0d18a
Dustin Pierson <dustinapierson@gmail.com>
2018-01-12 16:18:37 -0600

    finished ul section

--- DOM/app.js
+++ DOM/app.js
@@ -1,7 +1,7 @@
 
 
-let btn = document.createElement('button')
-let btnTxt = document.createTextNode("Click Me")
+let btn = document.createElement('button');
+let btnTxt = document.createTextNode("Click Me");
 document.body.appendChild(btn);
 btn.appendChild(btnTxt);
 
@@ -16,10 +16,19 @@ myDiv.style.height = '3em';
 myDiv.style.width = '3em';
 
 let para = document.createElement('p')
-let paraTxt = document.createTextNode('This is my paragraph text, not too long, not too short, just right!')
+let paraTxt = document.createTextNode('This is my paragraph text, not too long, not too short, just right!');
 document.body.appendChild(para);
 para.appendChild(paraTxt);
 
+let emptyDiv = document.createElement('div');
+document.body.appendChild(emptyDiv);
+let divBtn = document.createElement('button');
+let divBtnTxt = document.createTextNode('Make Span');
+document.body.appendChild(divBtn);
+divBtn.appendChild(divBtnTxt);
+
+let ulBtn = document.getElementById("ulBtn");
+let fList = document.getElementById("fList");
 
 btn.addEventListener('click', function () {
     alert('What a Nice Message');
@@ -69,7 +78,26 @@ para.addEventListener('click', function () {
     let x = getRandomInt(5);
     para.style.color = colors[x].color
 
+})
 
+divBtn.addEventListener('click', function () {
+    let mySpan = document.createElement('span');
+    let mySpanTxt = document.createTextNode(' Dustin A Pierson ');
+    emptyDiv.appendChild(mySpan);
+    mySpan.appendChild(mySpanTxt);
 
 
 })
+ulBtn.addEventListener('click', function () {
+    let family = [' alysia ', ' joshua ', ' william ', ' katie ', ' rachel ', ' paul ', ' angie ', ' christy ', ' nathan ', ' hannah '];
+    
+
+    for (let x = 0; x < family.length; x++) {
+        let li = document.createElement('li')
+        let liTxt = document.createTextNode(family[x])
+        fList.appendChild(li);
+        li.appendChild(liTxt);
+
+    }
+
+});
